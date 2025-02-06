635aef1991f41a8922a08939d9b098fa82aecc63
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2025-02-06 14:04:06 +0200

    fix solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -34,13 +34,21 @@ class Carnivore extends Animal {
 
     Animal.alive.forEach((animal) => {
       if (animal === target) {
-        if (animal.hidden !== true && animal instanceof Herbivore) {
+        if (animal.hidden !== true) {
           animal.health -= BITE_POWER;
 
           if (animal.health <= 0) {
-            Animal.alive.splice(Animal.alive.indexOf(animal), 1);
+            Animal.alive = Animal.alive.filter((a) => a.health > 0);
           }
         }
+
+        // if (animal.hidden !== true && animal instanceof Herbivore) {
+        //   animal.health -= BITE_POWER;
+
+        //   if (animal.health <= 0) {
+        //     Animal.alive = Animal.alive.filter((a) => a.health > 0);
+        //   }
+        // }
       }
     });
   }
